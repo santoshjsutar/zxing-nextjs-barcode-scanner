ef132ff16577c5c31de51e7ff6803de915580ff7
Santosh JS <santoshjsutar@gmail.com>
2025-07-09 15:17:26 +0530

    remove fonts issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter", // Define a CSS variable name
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-inter", // Define a CSS variable name
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
